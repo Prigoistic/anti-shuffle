d7a0afeb27cbb1c63f9ad645f0d4c3badf81ddcd
Priyam Ghosh <priyamghosh9753@gmail.com>
2025-12-04 03:07:53 +0530

    init: Android app module setup with Gradle 8.5 wrapper, AGP 8.1.1, Kotlin 1.9.22; added Compose + Hilt boilerplate, README, and minimal root build file.

---
+++ app/src/main/java/com/gallery_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.gallery_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Text(text = "Smart Gallery Starting Point")
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,15 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
+    }
+
+    plugins {
+        id("com.android.application") version "8.1.1"
+        id("com.android.library") version "8.1.1"
+        id("org.jetbrains.kotlin.android") version "1.9.22"
+        id("com.google.dagger.hilt.android") version "2.50"
     }
 }
 
