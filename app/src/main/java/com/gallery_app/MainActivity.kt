0d555f0b10c330595dcaf658790e7ed3e5eab1e3
Priyam Ghosh <priyamghosh9753@gmail.com>
2025-12-06 04:56:12 +0530

    Added : Updated UI and gave permissions to Android Builds

--- app/src/main/java/com/gallery_app/MainActivity.kt
+++ app/src/main/java/com/gallery_app/MainActivity.kt
@@ -5,6 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.gallery_app.ui.PermissionScreen
+import com.gallery_app.ui.GalleryHomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +19,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Text(text = "Smart Gallery Starting Point")
+                var permissionGranted by remember { mutableStateOf(false) }
+                if(!permissionGranted){
+                    PermissionScreen{
+                        permissionGranted = true
+                    }
+                }else{
+                    GalleryHomeScreen()
+                }
+
             }
         }
     }
